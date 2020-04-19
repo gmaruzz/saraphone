7ececb4e835f1dcbf308bdc0dae07e648076b2e6
Giovanni Maruzzelli <gmaruzz@gmail.com>
2020-04-20 00:25:47 +0200

    app_languages: added it-it, Italian

--- app_languages.php
+++ app_languages.php
@@ -22,65 +22,96 @@ $text['title']['sv-se'] = "SaraPhone WebRTC";
 $text['title']['uk-ua'] = "SaraPhone WebRTC";
 
 $text['head_description']['en-us'] = "A WebRTC client for SIP";
+$text['head_description']['it-it'] = "WebRTC client per SIP";
 
 $text['choose_phone']['en-us'] = "Choose which Phone";
+$text['choose_phone']['it-it'] = "Scegli Telefono";
 
 $text['back_to_dashboard']['en-us'] = "Back to DashBoard";
+$text['back_to_dashboard']['it-it'] = "Torna al Pannello";
 
 $text['wait_please']['en-us'] = "Wait please...";
+$text['wait_please']['it-it'] = "Attendere prego...";
 
 $text['your_account_login']['en-us'] = "Your Account Login (eg: 2020)";
+$text['your_account_login']['it-it'] = "Inserisci il Login del tuo Account (es: 2020)";
 
 $text['your_account_password']['en-us'] = "Your Account Password (eg: 12345)";
+$text['your_account_password']['it-it'] = "Inserisci la Password del tuo Account (es: 12345)";
 
 $text['your_display_name']['en-us'] = "Your Display Name (optional, eg: Giovanni Maruzzelli)";
+$text['your_display_name']['it-it'] = "Inserisci il tuo Display Name (opzionale, es: Paolo Rossi)";
 
 $text['advanced_set_server_and_blfs']['en-us'] = "Advanced: set Server and BLFs";
+$text['advanced_set_server_and_blfs']['it-it'] = "Avanzato: Server e BLFs";
 
 $text['label']['en-us'] = "Label";
+$text['label']['it-it'] = "Etichetta";
 
 $text['placeholder_number_to_dial']['en-us'] = "number to dial (eg: 0549123456)";
+$text['placeholder_number_to_dial']['it-it'] = "numero da chiamare (es: 0549123456)";
 
 $text['answer']['en-us'] = "Answer";
+$text['answer']['it-it'] = "Rispondi";
 
 $text['reject']['en-us'] = "Reject";
+$text['reject']['it-it'] = "Rifiuta";
 
 $text['dial']['en-us'] = "Dial";
+$text['dial']['it-it'] = "Chiama";
 
 $text['cancel']['en-us'] = "Canc";
+$text['cancel']['it-it'] = "Canc";
 
 $text['redial']['en-us'] = "ReDial";
+$text['redial']['it-it'] = "RiChiama";
 
 $text['dnd']['en-us'] = "DND";
+$text['dnd']['it-it'] = "DND";
 
 $text['voicemail']['en-us'] = "VoiceMail";
+$text['voicemail']['it-it'] = "Messaggi";
 
 $text['contacts']['en-us'] = "Contacts";
+$text['contacts']['it-it'] = "Contatti";
 
 $text['mute_ring']['en-us'] = "MUTE RING";
+$text['mute_ring']['it-it'] = "MUTE RING";
 
 $text['autoanswer']['en-us'] = "AUTOANSWER";
+$text['autoanswer']['it-it'] = "AUTOANSWER";
 
 $text['audio_microphone']['en-us'] = "Audio/Mic";
+$text['audio_microphone']['it-it'] = "Audio/Mic";
 
 $text['allow_notifications']['en-us'] = "Allow Call Notifications";
+$text['allow_notifications']['it-it'] = "Permetti Notifiche Chiamate";
 
 $text['speaking_with']['en-us'] = "Speaking with";
+$text['speaking_with']['it-it'] = "In conversazione";
 
 $text['hangup']['en-us'] = "Hangup";
+$text['hangup']['it-it'] = "Chiudi";
 
 $text['hold']['en-us'] = "Hold";
+$text['hold']['it-it'] = "Hold";
 
 $text['mute']['en-us'] = "Mute";
+$text['mute']['it-it'] = "Muto";
 
 $text['att_xfer']['en-us'] = "Att-Xfer";
+$text['att_xfer']['it-it'] = "Att-Xfer";
 
 $text['xfer']['en-us'] = "Xfer";
+$text['xfer']['it-it'] = "Xfer";
 
 $text['blfs']['en-us'] = "BLFs";
+$text['blfs']['it-it'] = "BLFs";
 
 $text['attxbtn_title']['en-us'] = "at beep, enter the digits you want to call, then press '#'. You will be connected. After you talk, hangup to transfer the original call, or press '#' to get the original call back. If attended transfer does not work, you get the original call back.";
+$text['attxbtn_title']['it-it'] = "al beep, inserire il numero da chiamare, poi premere '#'. Verrete connessi. Dopo aver parlato, riagganciare per trafserire la chiamata originale, o premere '#' per riprendere la chiamata originale. Se il trasferimento fallisce, riavrete la chiamata originale.";
 
 $text['xferbtn_title']['en-us'] = "at beep, enter the digits you want to call, then press '#'. If transfer does not work, you get the original call back.";
+$text['xferbtn_title']['it-it'] = "al beep, inserire il numero da chiamare, poi premere '#'. Se il trasferimento fallisce, riavrete la chiamata originale.";
 
 ?>
