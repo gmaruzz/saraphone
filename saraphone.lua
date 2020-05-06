4a015d598273a48e21b81c9134b1a09c1a1f12c7
Giovanni Maruzzelli <gmaruzz@opentelecom.it>
2020-05-06 11:12:54 +0200

    always bind_digit_action, for all kind of SIP clients, more complex star codes (feature codes) so they can be used by all SIP clients with less overlapping danger with online services

--- saraphone.lua
+++ saraphone.lua
@@ -42,7 +42,7 @@ freeswitch.consoleLog(loglevel, uuid .. " saraphone_caller_user_agent: " .. sara
 freeswitch.consoleLog(loglevel, uuid .. " saraphone_bind: " .. saraphone_bind .. "\n");
 
 if(saraphone_bind == "false") then
-session:setVariable("saraphone_bind", "true");
+session:execute("export","saraphone_bind=true");
 
 local saraphone_is_destination = string.find(saraphone_destination_user_agent, "SaraPhone");
 if(saraphone_is_destination) then
@@ -54,13 +54,8 @@ end
 local saraphone_is_caller = string.find(saraphone_caller_user_agent, "SaraPhone");
 if(saraphone_is_caller) then
 	saraphone_is_caller = "true"
-	-- XXX why next line breaks call ? XXX
-	--session:execute("export","absolute_codec_string='PCMA,PCMU'");
 	session:execute("export","ignore_early_media=false");
-	--session:setVariable("ringback", "${us-ring}");
 	session:setVariable("ringback", saraphone_ringback);
-	-- XXX why next line breaks call ? XXX
-	--session:setVariable("transfer_ringback", "${us-ring}");
 	session:setVariable("instant_ringback", "true");
 	session:answer()
 	api:execute("msleep", "1000");
@@ -68,43 +63,15 @@ else
 	saraphone_is_caller = "false"
 end
 
-local saraphone_is_both = "false"
-if( saraphone_is_destination == "true" and saraphone_is_caller == "true" ) then
-	saraphone_is_both ="true"
-	saraphone_is_destination ="false"
-	saraphone_is_caller ="false"
-else
-	saraphone_is_both ="false"
-end
+session:execute("export","saraphone_is_both=true");
 
-freeswitch.consoleLog(loglevel, uuid .. " saraphone_is_destination: " .. saraphone_is_destination .. "\n");
-freeswitch.consoleLog(loglevel, uuid .. " saraphone_is_caller: " .. saraphone_is_caller .. "\n");
-freeswitch.consoleLog(loglevel, uuid .. " saraphone_is_both: " .. saraphone_is_both .. "\n");
-
-session:execute("export","saraphone_is_destination=" .. saraphone_is_destination);
-session:execute("export","saraphone_is_caller=" .. saraphone_is_caller);
-session:execute("export","saraphone_is_both=" .. saraphone_is_both);
-
-if(saraphone_is_both == "true") then
-	session:execute("bind_digit_action","saraphone_local,*2,exec:execute_extension,saraphone_hold XML ${context},aleg,bleg");
-	session:execute("bind_digit_action","saraphone_local,*3,exec:execute_extension,saraphone_hold XML ${context},peer,peer");
-	session:execute("bind_digit_action","saraphone_local,*4,exec:execute_extension,saraphone_dx XML ${context},aleg,bleg");
-	session:execute("bind_digit_action","saraphone_local,*5,exec:execute_extension,saraphone_dx XML ${context},peer,peer");
-	session:execute("bind_digit_action","saraphone_local,*6,exec:execute_extension,saraphone_att_xfer XML ${context},aleg,bleg");
-	session:execute("bind_digit_action","saraphone_local,*7,exec:execute_extension,saraphone_att_xfer XML ${context},peer,peer");
+	session:execute("bind_digit_action","saraphone_local,*299,exec:execute_extension,saraphone_hold XML ${context},aleg,bleg");
+	session:execute("bind_digit_action","saraphone_local,*399,exec:execute_extension,saraphone_hold XML ${context},peer,peer");
+	session:execute("bind_digit_action","saraphone_local,*499,exec:execute_extension,saraphone_dx XML ${context},aleg,bleg");
+	session:execute("bind_digit_action","saraphone_local,*599,exec:execute_extension,saraphone_dx XML ${context},peer,peer");
+	session:execute("bind_digit_action","saraphone_local,*699,exec:execute_extension,saraphone_att_xfer XML ${context},aleg,bleg");
+	session:execute("bind_digit_action","saraphone_local,*799,exec:execute_extension,saraphone_att_xfer XML ${context},peer,peer");
 	session:execute("digit_action_set_realm","saraphone_local");
-elseif(saraphone_is_caller == "true") then
-	session:execute("bind_digit_action","saraphone_local,*2,exec:execute_extension,saraphone_hold XML ${context},aleg,bleg");
-	session:execute("bind_digit_action","saraphone_local,*4,exec:execute_extension,saraphone_dx XML ${context},aleg,bleg");
-	session:execute("bind_digit_action","saraphone_local,*6,exec:execute_extension,saraphone_att_xfer XML ${context},aleg,bleg");
-	session:execute("digit_action_set_realm","saraphone_local");
-elseif(saraphone_is_destination == "true") then
-	session:execute("bind_digit_action","saraphone_local,*3,exec:execute_extension,saraphone_hold XML ${context},peer,peer");
-	session:execute("bind_digit_action","saraphone_local,*5,exec:execute_extension,saraphone_dx XML ${context},peer,peer");
-	session:execute("bind_digit_action","saraphone_local,*7,exec:execute_extension,saraphone_att_xfer XML ${context},peer,peer");
-	session:execute("digit_action_set_realm","saraphone_local");
-end
-
 end
 
 freeswitch.consoleLog(loglevel, uuid .. " ------------ END   ----------------------------------------------------------\n")
